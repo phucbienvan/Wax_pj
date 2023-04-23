97b192fa6cd6d2ea027db4f1d8bd83312b1bcd69
phucbienvan <phuc.bienvan@gmail.com>
2023-04-24 01:12:58 +0700

    add test case wax

--- test/Wax.test.js
+++ test/Wax.test.js
@@ -7,11 +7,31 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 var wax = artifacts.require("Wax.sol");
 
-contract("Wax", async(account) => {
+contract("Wax", async(accounts) => {
+    const [
+            owner,
+            accountOne
+        ] = accounts;
+    
+    console.log('Deployer: ', owner);
+
     it("All token should be in first account", async () => {
         let instance = await wax.deployed();
         let totalSupply = await instance.totalSupply();
-        console.log(totalSupply);
-        expect(await instance.balanceOf(account[0])).to.be.bignumber.equal(totalSupply);
-    })
+        expect(await instance.balanceOf(owner)).to.be.bignumber.equal(totalSupply);
+    });
+
+    it("Is possible send token", async () => {
+        const sendToken = 1000;
+        let instance = await wax.deployed();
+        let totalSupply = await instance.totalSupply();
+
+        await expect(instance.balanceOf(owner)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.transfer(accountOne, sendToken)).to.eventually.be.fulfilled;
+        await expect(instance.balanceOf(owner)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendToken)));
+
+        // await expect(instance.balanceOf(accountOne)).to.be.bignumber.equal(new BN(sendToken));
+
+        await expect(instance.balanceOf(accountOne)).to.eventually.be.a.bignumber.equal(new BN(sendToken));
+    });
 });
